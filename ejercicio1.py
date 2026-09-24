cc349253e0325b6e3b8861a89ad8101336405e8b
Gaston <gaston98.bravo@gmail.com>
2026-09-23 23:26:15 -0300

    mejora

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,5 +1,5 @@
 def fizzbuzz(numero):
-    """Recibe un número y devuelve el string correspondiente según las reglas de FizzBuzz."""
+   
     if numero % 15 == 0:
         return "FizzBuzz"
     elif numero % 3 == 0:
